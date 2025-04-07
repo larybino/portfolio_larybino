5856b66bb9325502f9d5a939fc1593e857f0a032
larybino <binolaryssa@gmail.com>
2025-04-07 15:25:50 -0300

    adicionando funcionalidade modo noturno/diurno

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const rootHtml = document.documentElement
+const toggleTheme = document.getElementById("toggleTheme");
 const accordionHeaders = document.querySelectorAll(".accordion__header");
 const menuLinks = document.querySelectorAll(".menu__link");
+
+
+function changeTheme() {
+  const currentTheme = document.body.getAttribute("data-theme");
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
+  localStorage.setItem("tema", newTheme);
+  document.body.setAttribute("data-theme", newTheme);
+
+  document.getElementById("iconSun").style.display = newTheme === "dark" ? "inline" : "none";
+  document.getElementById("iconMoon").style.display = newTheme === "dark" ? "none" : "inline";
+}
+
 
 accordionHeaders.forEach(header => {
   header.addEventListener("click", () => {
@@ -18,14 +30,42 @@ menuLinks.forEach(item => {
   })
 })
 
-async function buscarVersiculo() {
+async function searchVerse() {
   try {
       const response = await fetch("https://bible-api.com/?random=verse");
       const data = await response.json();
-      document.getElementById("versiculo").innerText = `"${data.text}" - ${data.reference}`;
+      document.getElementById("verse").innerText = `"${data.text}" - ${data.reference}`;
   } catch (error) {
       console.error("Erro ao buscar o versículo:", error);
-      document.getElementById("versiculo").innerText = "Não foi possível carregar um versículo.";
+      document.getElementById("verse").innerText = "Não foi possível carregar um versículo.";
   }
 }
-document.addEventListener("DOMContentLoaded", buscarVersiculo);
+document.addEventListener("DOMContentLoaded", searchVerse);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const tema = localStorage.getItem("tema") || "dark";
+  document.body.setAttribute("data-theme", tema);
+  atualizarIcone(tema);
+
+  document.getElementById("toggleTheme").addEventListener("click", () => {
+    const currentTheme = document.body.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    document.body.setAttribute("data-theme", newTheme);
+    localStorage.setItem("tema", newTheme);
+    atualizarIcone(newTheme);
+  });
+});
+
+function atualizarIcone(tema) {
+  const sun = document.getElementById("iconSun");
+  const moon = document.getElementById("iconMoon");
+  if (tema === "dark") {
+    sun.style.display = "inline";
+    moon.style.display = "none";
+  } else {
+    sun.style.display = "none";
+    moon.style.display = "inline";
+  }
+}
+
+
